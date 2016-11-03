3e7bc74062039dd8418276da2cf39adf66a1b320
Lee Machin <lee@typeform.com>
2016-11-03 21:25:42 +0100

    Add the implementation and hope it works!

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 var botBuilder = require('claudia-bot-builder')
-var gh = require('github')
+var GithubApi = require('github')
 
-module.exports = botBuilder(function (msg) {
-  return 'lol'
+var env = require('process').env
+
+var gh = new GithubApi({
+  protocol: 'https',
+  host: 'api.github.com',
+  headers: {
+    'user-agent': 'github-repo-search-slack'
+  }
+})
+
+module.exports = botBuilder(function (name, apiReq) {
+  gh.authenticate({
+    type: 'token',
+    token: env.GITHUB_TOKEN
+  })
+
+  return Promise.all(
+    [
+      gh.repos.get({
+        owner: 'typeform',
+        repo: name
+      }),
+      gh.repos.getReadme({
+        owner: 'typeform',
+        repo: name,
+        headers: {
+          'Accept': 'application/vnd.github.VERSION.raw'
+        }
+      })
+    ])
+    .then(function (repo, readme) {
+      var msg = [
+        repo.name,
+        repo.description,
+        repo.html_url,
+        '```',
+        readme,
+        '```',
+        ':yeah:'
+      ]
+
+      return msg.join('\n')
+    })
+    .catch(function (err) {
+      return err + ':sadpanda: :facepalm:'
+    })
 })

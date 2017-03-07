0fd37693725140670ba573bb913626a31855aea5
Lee Machin <lee@typeform.com>
2017-03-07 16:45:05 +0100

    Move to ES6 and split things into modules

---
+++ src/cmd/validate.js
@@ -0,0 +1,72 @@
+var validator = require('ajv')
+
+
+var schema = {
+  team: {
+    type: 'object',
+    required: true,
+    properties: {
+      name: { type: 'string' },
+      slack_channel: { type: 'string' }
+    }
+  },
+  deploy_url: {
+    type: 'url',
+    required: true
+  },
+  ci_url: {
+    type: 'url',
+    required: true
+  },
+  name: {
+    type: 'string',
+    required: false
+  },
+  description: {
+    type: 'string',
+    required: false
+  },
+  service_url: {
+    type: 'url',
+    required: false
+  },
+  docs: {
+    type: 'array',
+    required: false,
+    properties: {
+      title: { type: 'string', required: false },
+      url: { type: 'url' }
+    }
+  },
+  dependencies: {
+    type: 'object',
+    required: false,
+    properties: {
+      name: { type: 'string' },
+      url: { type: 'url' }
+    }
+  },
+  ports: {
+    type: 'object',
+    required: false
+  },
+  infrastructure: {
+    type: 'object',
+    required: false
+  },
+  aws: {
+    type: 'object',
+    required: false
+
+  },
+  metrics: {
+    type: 'object',
+    required: false
+
+  }
+}
+
+
+module.exports = function (payload) {
+
+}
---
+++ src/format.js
@@ -0,0 +1,66 @@
+export default class Format {
+
+  constructor (repo, metadata) {
+    this.repo = repo
+    this.metadata = metadata
+  }
+
+  get separator () {
+    return '\n--------\n'
+  }
+
+  name () {
+    return `*${this.metadata.name || this.repo.name}*`
+  }
+
+  description () {
+    return `_${this.metadata.description || this.repo.description}_`
+  }
+
+  siteUrls () {
+    if (this.metadata.service_url) {
+      return `${this.metadata.service_url} | ${this.repo.html_url}`
+    } else {
+      return this.repo.html_url
+    }
+  }
+
+  team () {
+    const {name, slack_channel} = this.metadata.team
+
+    return `Team: ${name} (#${slack_channel}) :yeah:`
+  }
+
+  deployment () {
+    const {ci_url, deploy_url} = this.metadata
+
+    if (ci_url && deploy_url) {
+      return [`CI: ${ci_url}`, `Deploy: ${deploy_url}`].join('\n')
+    }
+  },
+
+  docs () {
+    const {docs} = this.metadata.docs
+
+    if (docs) {
+      const links = docs.map(doc => {
+        if (doc.title) {
+          return `<${doc.url}|${doc.title}>`
+        } else {
+          return doc.url
+        }
+      })
+
+      return ['Docs:', ...links].join('\n')
+    }
+  }
+
+  deps () {
+    const {dependencies: deps} = this.metadata
+
+    if (deps) {
+      const links = deps.map(dep => `<${dep.url}|${dep.name}>`)
+      return ['Dependencies:', ...links].join('\n')
+    }
+  }
+}
